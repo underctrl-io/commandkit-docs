3765e9e223f553101d1c7751b12e03d461fbe1ba
notunderctrl <underctrl.io@gmail.com>
2023-07-04 19:31:12 +0300

    add `site` to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://commandkit.underctrl.io',
   integrations: [
     starlight({
       title: 'CommandKit',
