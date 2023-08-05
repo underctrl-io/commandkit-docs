a7c9a03db5f453f36b88b9b549b28e9266bbb573
notunderctrl <avraj@underctrl.io>
2023-08-05 09:48:10 +0300

    update github url to match organization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,36 +3,39 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://commandkit.underctrl.io',
-  integrations: [
-    starlight({
-      title: 'CommandKit',
-      social: {
-        github: 'https://github.com/notunderctrl/commandkit',
-        discord: 'https://discord.underctrl.io',
-        youtube: 'https://youtube.com/@UnderCtrl',
-      },
-      sidebar: [
-        { label: 'Overview', link: '/overview' },
-        {
-          label: 'Guides',
-          items: [
-            { label: 'Installation', link: '/guides/installation' },
-            { label: 'CommandKit Setup', link: '/guides/commandkit-setup' },
-            { label: 'Commands Setup', link: '/guides/command-file-setup' },
-            { label: 'Events Setup', link: '/guides/event-file-setup' },
-            { label: 'Validations Setup', link: '/guides/validation-file-setup' },
-            { label: 'Migrate from DJS-Commander', link: '/guides/migrating-from-djs-commander' },
-          ],
-        },
-        {
-          label: 'Documentation',
-          items: [{ label: 'Introduction', link: '/docs/introduction' }],
-        },
-      ],
-    }),
-  ],
+    site: 'https://commandkit.underctrl.io',
+    integrations: [
+        starlight({
+            title: 'CommandKit',
+            social: {
+                github: 'https://github.com/underctrl-io/commandkit',
+                discord: 'https://discord.underctrl.io',
+                youtube: 'https://youtube.com/@UnderCtrl',
+            },
+            sidebar: [
+                { label: 'Overview', link: '/overview' },
+                {
+                    label: 'Guides',
+                    items: [
+                        { label: 'Installation', link: '/guides/installation' },
+                        { label: 'CommandKit Setup', link: '/guides/commandkit-setup' },
+                        { label: 'Commands Setup', link: '/guides/command-file-setup' },
+                        { label: 'Events Setup', link: '/guides/event-file-setup' },
+                        { label: 'Validations Setup', link: '/guides/validation-file-setup' },
+                        {
+                            label: 'Migrate from DJS-Commander',
+                            link: '/guides/migrating-from-djs-commander',
+                        },
+                    ],
+                },
+                {
+                    label: 'Documentation',
+                    items: [{ label: 'Introduction', link: '/docs/introduction' }],
+                },
+            ],
+        }),
+    ],
 
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+    // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+    image: { service: { entrypoint: 'astro/assets/services/sharp' } },
 });
